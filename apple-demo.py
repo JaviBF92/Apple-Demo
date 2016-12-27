29ca3a5d7a7e24889c88f72675db8390fa786f5b
JaviBF92 <javibf1992@gmail.com>
2016-12-28 00:22:51 +0100

    name changed in dictionary, bug fixed

--- apple-demo.py
+++ apple-demo.py
@@ -3,7 +3,8 @@ import json
 #print(Hook['params'])
 #print(Hook['req'])
 
-database = {"products":[{"product-name":"MacBook",
+
+database = {"products":[{"name":"MacBook",
                     "category":"Mac",
                     "image":"http://store.storeimages.cdn-apple.com/4662/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/select/macbook-select-spacegray-201604?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1473974029537",
                     "link":"http://www.apple.com/es/macbook/",
@@ -17,7 +18,7 @@ database = {"products":[{"product-name":"MacBook",
                     "size":"1.31 x 28.05 x 19.65",
                     "weight":0.92
 					},
-                   {"product-name":"MacBook Air",
+                   {"name":"MacBook Air",
                     "category":"Mac",
                     "image":"",
                     "link":"http://www.apple.com/es/macbook-air/",
@@ -31,7 +32,7 @@ database = {"products":[{"product-name":"MacBook",
                     "size":"1.7 x 32.5 x 22.7",
                     "weight":1.35
 					},
-                   {"product-name":"iMac",
+                   {"name":"iMac",
                     "category":"Mac",
                     "image":"",
                     "link":"http://www.apple.com/es/imac/",
@@ -62,7 +63,7 @@ database = {"products":[{"product-name":"MacBook",
 def find_products(item, find_by, more_info=False):
 	products = [i for i in database["products"] if str(item) in i[find_by].lower()]
 	if not more_info:
-		products = [{key:i[key] for key in ["product-name","image","link","prices"]} for i in products]
+		products = [{key:i[key] for key in ["name","image","link","prices"]} for i in products]
 	return products
 
 request = Hook['params']
@@ -71,12 +72,12 @@ status = 'ok'
 
 if 'rq' in request:
     rq = request['rq']
-    if rq in [i["name"] for i in database['categories']]:
+    if rq in [i["name"].lower() for i in database['categories']]:
         body = find_products(rq,"category")
 
     elif rq == 'search':
         if 'search' in request:
-            body = find_products(request['search'],"product-name")
+            body = find_products(request['search'],"name")
             if not body:
                 body = "empty"
 
@@ -86,7 +87,7 @@ if 'rq' in request:
 
     elif rq == 'info':
         if 'prod' in request:
-            body = find_products(request['prod'], "product_name", True)
+            body = find_products(request['prod'], "name", True)
         else:
             status = 'error'
             body = 'wrong parameters in request'
