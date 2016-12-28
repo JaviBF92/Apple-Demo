a0256d268260d1923225b66ad47298ce9525bc76
JaviBF92 <javibf1992@gmail.com>
2016-12-28 12:06:22 +0100

    Some names in database changed; function get_additional_info done

--- apple_demo.py
+++ apple_demo.py
@@ -1,51 +1,124 @@
+# -*- coding: utf-8 -*-
 import json
 
 
 #print(Hook['req'])
-#Hook={"params":{"rq":"search","search":"im"}}
+#Hook={"params":{"rq":"info_MacBook"}}
 
 database = {"products":[{"title":"MacBook",
                     "category":"Mac",
                     "image_url":"http://store.storeimages.cdn-apple.com/4662/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/select/macbook-select-spacegray-201604?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1473974029537",
                     "link":"http://www.apple.com/es/macbook/",
                     "price":"1499 €",
-                    "screen-size":12,
-                    "processor":{"processor-name":"Intel Core m3",
-                                "cores":2,
-                                "freq":"1.1 GHz"},
-                    "ram":"8 GB LPDDR3",
-                    "disk-size":"256 GB",
-                    "size":"1.31 x 28.05 x 19.65",
-                    "weight":0.92
+                    "Screen size":12,
+                    "Processor":"Intel Core m3 Dual Core, 1.1 GHz",
+                    "RAM":"8 GB LPDDR3",
+                    "Capacity":"256 GB",
+                    "Size":"1.31 x 28.05 x 19.65",
+                    "Weight":"0.92 Kg"
 					},
                    {"title":"MacBook Air",
                     "category":"Mac",
                     "image_url":"http://store.storeimages.cdn-apple.com/4662/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ac/macbook/air/macbook-air-gallery5-2014?wid=1292&hei=766&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1476297407895",
                     "link":"http://www.apple.com/es/macbook-air/",
                     "price":"1099 €",
-                    "screen-size":13.3,
-					"processor":{"processor-name":"Intel Core i5",
-                                "cores":2,
-                                "freq":"1.6 GHz"},
-                    "ram":"8 GB LPDDR3",
-                    "disk-size":"256 GB",
-                    "size":"1.7 x 32.5 x 22.7",
-                    "weight":1.35
+                    "Screen size":13.3,
+					"Processor":"Intel Core i5 Dual Core, 1.6 GHz",
+                    "RAM":"8 GB LPDDR3",
+                    "Capacity":"256 GB",
+                    "Size":"1.7 x 32.5 x 22.7",
+                    "Weight":"1.35 Kg"
 					},
                    {"title":"iMac",
                     "category":"Mac",
                     "image_url":"http://store.storeimages.cdn-apple.com/4662/as-images.apple.com/is/image/AppleInc/aos/published/images/I/MA/IMAC/IMAC?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=dSMkx2",
                     "link":"http://www.apple.com/es/imac/",
                     "price":"1279 €",
-                    "screen-size":21.5,
-					"processor":{"processor-name":"Intel Core i5",
-                                "cores":2,
-                                "freq":"1.6 GHz"},
-                    "ram":"8 GB LPDDR3",
-                    "disk-size":"1 TB",
+                    "Screen size":21.5,
+					"Processor":"Intel Core i5 Dual Core, 1.6 GHz",
+                    "RAM":"8 GB LPDDR3",
+                    "Capacity":"1 TB",
                     "GPU":"Intel HD Graphics 6000",
-                    "size":"45 x 52.8 x 17.5",
-                    "weight":5.68
+                    "Size":"45 x 52.8 x 17.5",
+                    "Weight":"5.68 Kg"
+					},
+                   {"title":"iPad Pro",
+                    "category":"iPad",
+                    "image_url":"http://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/i/pa/ipad/pro/ipad-pro-select-spacegray-201603_GEO_US?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1481049028689",
+                    "link":"http://www.apple.com/es/ipad-pro/",
+                    "price":"679 €",
+                    "Screen size":9.7,
+					"Processor":"A9X",
+                    "RAM":"2 GB",
+                    "Capacity":"32 GB",
+                    "Size":"24 x 16.95 x 0.61",
+                    "Weight":"437 g",
+                    "Camera":"12 Mpx"
+					},
+                   {"title":"iPad Air 2",
+                    "category":"iPad",
+                    "image_url":"https://www.apple.com/euro/ipad-air-2/a/generic/images/og.jpg?201611302133",
+                    "link":"http://www.apple.com/es/ipad-air-2/",
+                    "price":"429 €",
+                    "Screen size":9.7,
+					"Processor":"A8X",
+                    "RAM":"2 GB",
+                    "Capacity":"32 GB",
+                    "Size":"24 x 16.95 x 0.61",
+                    "Weight":"444 g",
+                    "Camera":"12 Mpx"
+					},
+                   {"title":"iPad mini 4",
+                    "category":"iPad",
+                    "image_url":"http://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/m/ob/mobile/ipad/mobile-ipad-mini-4-hero-2015_GEO_MX?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1474916038114",
+                    "link":"http://www.apple.com/es/ipad-mini-4/",
+                    "price":"429 €",
+                    "Screen size":7.9,
+					"Processor":"A8",
+                    "RAM":"2 GB",
+                    "Capacity":"32 GB",
+                    "Size":"24 x 16.95 x 0.61",
+                    "Weight":"299 g",
+                    "Camera":"8 Mpx"
+					},
+                   {"title":"iPhone 7",
+                    "category":"iPhone",
+                    "image_url":"https://www.o2.co.uk/shop/homepage/images/shop15/brand/apple/iphone7/apple-iphone-7-gallery-img-0.jpg",
+                    "link":"http://www.apple.com/es/iphone-7/",
+                    "price":"769 €",
+                    "Screen size":4.7,
+					"Processor":"A10",
+                    "RAM":"2 GB",
+                    "Capacity":"32 GB",
+                    "Size":"13.83 x 6.71 x 0.71",
+                    "Weight":"138 g",
+                    "Camera":"12 Mpx"
+					},
+                   {"title":"iPhone 6 S",
+                    "category":"iPhone",
+                    "image_url":"http://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone6s/select/iphone6s-select-2015?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=WkFkh3",
+                    "link":"http://www.apple.com/es/iphone6s/",
+                    "price":"659 €",
+                    "Screen size":4.7,
+					"Processor":"A9",
+                    "RAM":"2 GB",
+                    "Capacity":"32 GB",
+                    "Size":"13.83 x 6.71 x 0.71",
+                    "Weight":"143 g",
+                    "Camera":"12 Mpx"
+					},
+                   {"title":"iPhone SE",
+                    "category":"iPhone",
+                    "image_url":"http://store.storeimages.cdn-apple.com/4662/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphonese/select/iphonese-select-2016_FMT_WHH?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=p6Dlf3",
+                    "link":"http://www.apple.com/es/iphone-se/",
+                    "price":"489 €",
+                    "Screen size":4,
+					"Processor":"A9",
+                    "RAM":"2 GB",
+                    "Capacity":"16 GB",
+                    "Size":"12.38 x 5.86 x 0.76",
+                    "Weight":"113 g",
+                    "Camera":"12 Mpx"
 					}
                   ],
 			"categories":[{"name":"Mac",
@@ -104,11 +177,13 @@ def get_additional_info(product):
 
     if info:
         info = info[0]
-
+        msg = [info[key] for key in ["title","price"]]
+        msg = msg + [key + ": " + str(info[key]) for key in info if key not in ["title","price","link","category","image_url"]]
+        text="\n".join(msg)
     else:
-        info = None
+        text = None
 
-    return info
+    return text
 
 
 request = Hook['params']
